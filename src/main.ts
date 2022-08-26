92e69218cdd463d0dce66b5c6ee2d640279c6780
Histman7 <hsandovalt1900@alumno.ipn.mx>
2022-08-26 14:34:32 -0500

    Modified files

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
-import './style.css'
+import "./style.css";
 
+const rateStart = document.querySelector<HTMLDivElement>(".raiting-state");
+const thanYou = document.querySelector<HTMLDivElement>(".thank-you");
+const form = document.querySelector<HTMLFormElement>(".rating-form");
 
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const valueRate = document.querySelector("input[name=ratings]:checked");
 
+  if (valueRate !== null) {
+    const selectedRating =
+      document.querySelector<HTMLParagraphElement>(".value");
+    selectedRating!.textContent =
+      "You selected " + valueRate.getAttribute("value") + " out of 5";
+    rateStart!.classList.add("hidden");
+    thanYou!.classList.remove("hidden");
+  }
+});
